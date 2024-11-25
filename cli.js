dd4b31c4dd052f43ec000afddf7616d52e61126d
Чуриков Антон Алексеевич <stack.anton.churikov@hotmail.com>
2024-11-25 12:07:18 +0300

    fix

--- cli.js
+++ cli.js
@@ -6,8 +6,13 @@ const path = require('path');
 // Определяем путь к файлу SVG
 const svgPath = path.join(process.cwd(), 'resources', 'splash.svg');
 
+// Определяем путь к конфигу svgo
+const svgoConfigPath = path.join(process.cwd(), 'node_modules', 'svg2xml', 'svgo.config.js');
+
 // Команда для выполнения `svgo`
-const svgoCommand = `npx svgo --multipass --pretty ${svgPath} -o ${svgPath}`;
+const svgoCommand = `npx svgo --multipass --config ${svgoConfigPath} --pretty ${svgPath} -o ${svgPath}`;
+
+console.log();
 
 // Выполняем команду `svgo`
 exec(svgoCommand, (error, stdout, stderr) => {
