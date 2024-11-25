a76299b3f7763f9604341f3d6393ad230e96717b
Чуриков Антон Алексеевич <stack.anton.churikov@hotmail.com>
2024-11-25 10:18:37 +0300

    init

---
+++ svg2xml.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const path = require('path');
+const { DOMParser } = require('xmldom');
+
+// Функция для преобразования SVG в XML
+function svgToXml(svg) {
+  const parser = new DOMParser();
+  const svgDoc = parser.parseFromString(svg, "image/svg+xml");
+
+  const paths = svgDoc.getElementsByTagName("path");
+
+  let xml = `
+<vector xmlns:android="http://schemas.android.com/apk/res/android"
+        android:width="288dp"
+        android:height="288dp"
+        android:viewportWidth="288"
+        android:viewportHeight="288">`;
+
+    Array.from(paths).forEach((path) => {
+        const fillColor = path.getAttribute("fill") || "#000000";
+        const pathData = path.getAttribute("d") || "";
+        xml += `
+    <path
+            android:fillColor="${fillColor}"
+            android:pathData="${pathData}" />`;
+    });
+
+    xml += `
+</vector>`;
+
+    return xml.trim();
+}
+
+// Основная функция для чтения и записи файлов
+function main() {
+    const svgFilePath = path.join(process.cwd(), 'resources', 'splash.svg');
+    const xmlFilePath = path.join(process.cwd(), 'resources', 'splash.xml');
+
+  // Чтение SVG файла
+  fs.readFile(svgFilePath, 'utf8', (err, svgData) => {
+    if (err) {
+      console.error('Не удалось прочитать SVG файл:', err);
+      return;
+    }
+
+    // Преобразование SVG в XML
+    const xmlData = svgToXml(svgData);
+
+    // Запись XML в файл
+    fs.writeFile(xmlFilePath, xmlData, 'utf8', (err) => {
+      if (err) {
+        console.error('Не удалось записать XML файл:', err);
+      } else {
+        console.log('SVG успешно преобразован в XML и сохранен в ', xmlFilePath);
+      }
+    });
+  });
+}
+
+// Запуск основной функции
+main();
+
+export default svgToXml();
---
+++ svgo.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  plugins: [
+    {
+      name: 'preset-default',
+      params: {
+        overrides: {
+          removeViewBox: false
+        },
+      },
+    },
+    "convertStyleToAttrs",
+    {
+      name: "convertColors",
+      params: {
+        currentColor: false,
+        names2hex: true,
+        rgb2hex: true,
+        convertCase: "lower",
+        shorthex: false,
+        shortname: true
+      }
+    },
+    {
+      name: "inlineStyles",
+      params: {
+        onlyMatchedOnce: false
+      }
+    },
+    {
+      name: "removeAttrs",
+      params: {
+        attrs: ["g:*","path:id","path:data-name"],
+        elemSeparator: ":",
+        preserveCurrentColor: false
+      }
+    },
+  ],
+}
